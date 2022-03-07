b457e7b865f1424def268b1d7ce4a7561f26a877
Oskar Schöldström <m@oxy.fi>
2022-03-07 15:05:33 -0300

    Add Kinsta Page Cache Invalidator

---
+++ src/Invalidators/KinstaCacheInvalidator.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Genero\Sage\CacheTags\Invalidators;
+
+use Genero\Sage\CacheTags\Contracts\Invalidator;
+
+class KinstaCacheInvalidator implements Invalidator
+{
+    const THROTTLED_PATH = 'https://localhost/kinsta-clear-cache/v2/throttled';
+    const CLEAR_ALL_PATH = 'https://localhost/kinsta-clear-cache-all';
+
+    public function clear(array $urls): bool
+    {
+        if (defined('KINSTAMU_DISABLE_AUTOPURGE') && KINSTAMU_DISABLE_AUTOPURGE === true) {
+            return false;
+        }
+
+        $purgeRequest = collect($urls)
+            ->map(fn ($url) => str_replace(['http://', 'https://'], '', $url))
+            ->mapWithKeys(function ($url, $key) {
+                return ["single|$key" => $url];
+            })
+            ->all();
+
+        $purgeRequest = apply_filters('KinstaCache/purgeThrottled', $purgeRequest);
+        $response = wp_remote_post(self::THROTTLED_PATH, [
+            'sslverify' => false,
+            'timeout' => 5,
+            'body' => $purgeRequest,
+        ]);
+
+        return !is_wp_error($response);
+    }
+
+    public function flush(): bool
+    {
+        $response = wp_remote_get(self::CLEAR_ALL_PATH, [
+            'sslverify' => false,
+            'timeout' => 5,
+        ]);
+        return !is_wp_error($response);
+    }
+}
