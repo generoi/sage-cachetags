ea8432c21092407d0007a825b8a62cfeca70ec19
Oskar Schöldström <m@oxy.fi>
2023-05-29 14:49:42 -0300

    Add a Site action for tagging multisites

---
+++ src/Tags/SiteTags.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Genero\Sage\CacheTags\Tags;
+
+use WP_Site;
+
+class SiteTags
+{
+    /**
+     * Return cache tag for the current site
+     */
+    public static function sites($sites = null): array
+    {
+        if (is_string($sites) && $sites === 'any') {
+            $sites = get_sites(['fields' => 'ids']);
+        } elseif (is_numeric($sites)) {
+            $sites = [$sites];
+        } elseif (is_null($sites)) {
+            $sites = [get_current_blog_id()];
+        }
+
+        if (is_array($sites)) {
+            return collect($sites)
+                ->map(fn ($site) => $site instanceof WP_Site ? $site->blog_id : $site)
+                ->map(fn ($site) => sprintf('site:%d', $site))
+                ->all();
+        }
+
+        return [];
+    }
+}
