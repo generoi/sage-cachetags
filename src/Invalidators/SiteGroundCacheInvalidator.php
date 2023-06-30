07369b02787d299f0f8800abc936a95e0e156a00
thunderdw <85177135+thunderdw@users.noreply.github.com>
2023-06-30 13:18:30 -0400

    Add SiteGround Optimizer cache invalidator (#5)
    * Add SiteGround Optimizer cache invalidator
    
    * Typo, file name fixes

---
+++ src/Invalidators/SiteGroundCacheInvalidator.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Genero\Sage\CacheTags\Invalidators;
+
+use Genero\Sage\CacheTags\Contracts\Invalidator;
+
+class SiteGroundOptimizerCacheInvalidator implements Invalidator
+{
+    public function clear(array $urls, array $tags): bool
+    {
+        return collect($urls)
+            ->map(fn ($url) => \sg_cachepress_purge_cache($url))
+            ->reduce(fn ($result, $urlResult) => $urlResult ? $result : false, true);
+    }
+
+    public function flush(): bool
+    {
+        return \sg_cachepress_purge_cache() ?: false;
+    }
+}
