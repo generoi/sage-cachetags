f2f0e13a16b57f9b063991b2661b6b854df36169
Oskar Schöldström <m@oxy.fi>
2024-03-07 13:03:36 -0300

    Fix rest responses do not have http headers

--- src/Actions/HttpHeader.php
+++ src/Actions/HttpHeader.php
@@ -5,6 +5,7 @@ namespace Genero\Sage\CacheTags\Actions;
 use Genero\Sage\CacheTags\CacheTags;
 use Genero\Sage\CacheTags\Contracts\Action;
 use Roots\Acorn\Application;
+use WP_REST_Response;
 
 class HttpHeader implements Action
 {
@@ -23,10 +24,11 @@ class HttpHeader implements Action
             return;
         }
 
-        // Core registers wp_ob_end_flush_all() as a shutdown action 
+        // Core registers wp_ob_end_flush_all() as a shutdown action
         ob_start();
-        
+
         \add_action('wp_footer', [$this, 'addHttpHeader']);
+        \add_filter('rest_post_dispatch', [$this, 'restPostDispatch']);
     }
 
     public function addHttpHeader(): void
@@ -38,5 +40,19 @@ class HttpHeader implements Action
                 collect($this->cacheTags->get())->join(' ')
             ));
         }
+    }
+
+    /**
+     * REST API response headers
+     */
+    public function restPostDispatch(WP_REST_Response $response): WP_REST_Response
+    {
+        if ($header = $this->app->config->get('cachetags.http-header')) {
+            $headers = $response->get_headers();
+            $headers[$header] = collect($this->cacheTags->get())->join(' ');
+            $response->set_headers($headers);
+        }
+
+        return $response;
     }
 }
